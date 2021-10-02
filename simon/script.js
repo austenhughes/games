a74166349ba4bb5cbadeab4af5a4df657673e3e7
Austn Hughes <austen.hughes.91@gmail.com>
2021-10-01 19:33:53 -0600

    set up bones of simon game

---
+++ simon/script.js
@@ -0,0 +1,17 @@
+// get corect history from veriable 
+// add new number from math floor math random number
+// save to varible 
+// add new number to history of numbers
+// light up color block that matches number for 1 second
+
+// make color blocks clickalbe 
+// get vallue form click
+// add to varible 
+// compare what you have to what is stored in the corect history
+// repeat for each new number added 
+// if you miss a number game over 
+// if you do not miss number get point
+// when game is over compare to high scores in local storage
+//  if higher than the three high scores add score and initials
+
+// play again?
